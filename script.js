f456d4b4f35a0cf583a6fd8c14d75fca1b170d61
Bharti <bharti@Bhartis-MacBook-Air.local>
2024-05-16 13:58:31 +0530

    Nav header with hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    // debugger;
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
