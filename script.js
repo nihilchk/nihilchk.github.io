641778b23622b574fbde153da08051649738b7e1
Bharti <bharti@Bhartis-MacBook-Air.local>
2024-05-21 20:06:22 +0530

    Site is good enough to go

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    // debugger;
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
